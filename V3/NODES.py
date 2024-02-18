ccbe27a5b82270910dc283a96696b340cfdd3633
Tom <sc21tpc@leeds.ac.uk>
2024-02-18 22:54:50 +0000

    First Functional Build

---
+++ V3/NODES.py
@@ -0,0 +1,167 @@
+from queue import Queue
+"""
+
+######### Consumers, Producers & End Nodes #########
+There are End_Nodes
+Consumers and Producers are End_Nodes
+An End_Node has a name, a master and a buffer.
+Consumers can only recieve data.
+Producers can send data, they can also path-find.
+
+#### Routers ####
+A router has a name, a routing table, a direction and a slave.
+The direction is the entry in the routing to which the router will send all flits
+
+
+
+
+"""
+def FM(name):
+    global Consumers, Producers, Routers
+    if(name[0].upper() == "R"):
+        for x in Routers:
+            if(x.name == name):
+                return x
+    elif(name[0].upper() == "P"):
+        for x in Producers:
+            if(x.name == name):
+                return x
+    elif(name[0].upper() == "C"):
+        for x in Consumers:
+            if(x.name == name):
+                return x
+    raise Exception(f"No module called {name}")
+
+
+
+class End_Node:
+    def __init__(self,name):
+        self.name = name
+        self.master = self
+        self.data = Queue()
+    def clear(self):
+        while(self.data.empty() == False):
+            self.data.get()
+    def declare_master(self, master_name):
+        self.master = FM(master_name)
+
+class Consumer(End_Node):
+    def recieve(self, data):
+        self.data.put(data)
+
+class Producer(End_Node):
+    def __init__(self,name):
+        super().__init__(name)
+        self.holding = False
+    #This gives the producer some data
+    Consumer_Nearest_Router = {'Con0' : [2,0],
+                               'Con1' : [0,0],
+                               'Con2' : [1,2]}
+    
+    Producer_Nearest_Router = {'Prod0' : [2,2],
+                               'Prod1' : [1,0],
+                               'Prod2' : [0,2]}
+    
+    #I will keep this for now,
+    #But why Would a producer need to hold data ever?
+    def hold_on(self):
+        if(self.data.empty()):
+            self.holding = False
+        else:
+            self.holding = False
+
+
+    def produce_message(self, data, target):
+        #Include the header
+        path = self.find_path(target)
+        for x in path:
+            self.data.put(x)
+        #Include the data
+        for x in data:
+            self.data.put(x)
+        #Include the tail
+        self.data.put("FREE")
+        
+    
+    def find_path(self,dest):
+        path =[]
+        current = self.Producer_Nearest_Router[self.name].copy()
+        target = self.Consumer_Nearest_Router[dest].copy()
+        #sort out X
+        while(current[0] != target[0]):
+            if(current[0] > target[0]):
+                current[0] -= 1
+                path.append("LEFT")
+            elif(current[0] < target[0]):
+                current[0] += 1
+                path.append("RIGHT")
+        #sort out Y
+        while(current[1] != target[1]):
+            if(current[1] > target[1]):
+                current[1] -= 1
+                path.append("DOWN")
+            elif(current[1] < target[1]):
+                current[1] += 1
+                path.append("UP")
+        path.append(dest)
+        return path
+    
+    def send(self):
+        if(self.holding == False):
+            if(self.data.empty()):
+                return
+            self.master.recieve(self.data.get())
+
+class Router():
+    def __init__(self,name):
+        self.data = Queue()
+        self.name = name
+        self.holding = False
+        self.direction = "FREE"
+        self.routing = {'UP' : self,
+                        'DOWN' : self,
+                        'LEFT' : self,
+                        'RIGHT' : self,
+                        'SLAVE' : self}
+    def declare_table(self,UDLR,SLAVE = 0):
+        self.routing["UP"] = FM(UDLR[0])
+        self.routing["DOWN"] = FM(UDLR[1])
+        self.routing["LEFT"] = FM(UDLR[2])
+        self.routing["RIGHT"] = FM(UDLR[3])
+        if(SLAVE != 0):
+            self.routing["SLAVE"] = FM(SLAVE)
+
+    def recieve(self, flit):
+        if(self.direction == "FREE"):
+            if(flit in ["DOWN","LEFT","UP","RIGHT"]):
+                self.direction = flit
+            else:
+                self.direction = "SLAVE"
+                pass
+        else:
+            self.data.put(flit)
+
+    #Every router is told: 
+    #If you haven't got anything at the beginning of the clock cycle,
+    #Do not send anything this clock cycle, to keep the illusion of synchronisation
+    def hold_on(self):
+        if(self.data.empty()):
+            self.holding = True
+        else:
+            self.holding = False
+
+    def send(self):
+        if(self.direction == "FREE" or self.data.empty() or self.holding):
+            #If we don't have no direction or no data
+            #Don't do anything
+            return
+        #Otherwise
+        flit = self.data.get()
+        self.routing[self.direction].recieve(flit)
+        if(flit == "FREE"): self.direction = "FREE"
+
+
+Consumers = [Consumer("Con0"),Consumer("Con1"),Consumer("Con2")]
+Producers = [Producer("Prod0"),Producer("Prod1"),Producer("Prod2")]
+Routers = [Router("Rout0"),Router("Rout1"),Router("Rout2"),Router("Rout3"),
+           Router("Rout4"),Router("Rout5"),Router("Rout6"),Router("Rout7"), Router("Rout8")]
---
+++ V3/WORMHOLE.py
@@ -0,0 +1,78 @@
+from NODES import FM, Routers, Consumers, Producers
+import keyboard, time
+
+
+#How are you going to test this code?
+
+first_time = True
+clock_tick = 0
+def update_UI():
+    #When I make a GUI, this is just going to return a JSON of the status of the nodes.
+    global first_time, clock_tick
+    if not(first_time):
+        print('\033[F' * 22)
+    print(f"Clock Tick: {clock_tick}")
+    print(f"Producer 1: {Producers[1].data.queue}                ")
+    print(f"Consumer 2: {Consumers[2].data.queue}                ")
+    for x in Routers:
+        print(f"{x.name} : {str(x.direction)}                ")
+        print(f"Buffer: {list(x.data.queue)}                 ")
+    first_time = False
+    time.sleep(1)
+
+"""
+   P2  C2  P0
+    |   |   |
+    8---1---2
+    |   |   |
+    7---O---3
+    |   |   |
+    6---5---4
+    |   |   |
+    C1  P1  C0
+"""
+
+FM("Rout0").declare_table(["Rout1","Rout5","Rout7","Rout3"])
+FM("Rout1").declare_table(["Rout1","Rout0","Rout8","Rout2"],"Con2")
+FM("Rout2").declare_table(["Rout2","Rout3","Rout1","Rout2"],"Prod0")
+FM("Rout3").declare_table(["Rout2","Rout4","Rout0","Rout3"])
+FM("Rout4").declare_table(["Rout3","Rout4","Rout5","Rout4"],"Con0")
+FM("Rout5").declare_table(["Rout0","Rout5","Rout6","Rout4"],"Prod1")
+FM("Rout6").declare_table(["Rout7","Rout6","Rout6","Rout5"],"Con1")
+FM("Rout7").declare_table(["Rout8","Rout6","Rout7","Rout0"])
+FM("Rout8").declare_table(["Rout8","Rout7","Rout8","Rout1"],"Prod2")
+FM("Con0").declare_master("Rout4")
+FM("Con1").declare_master("Rout6")
+FM("Con2").declare_master("Rout1")
+FM("Prod0").declare_master("Rout2")
+FM("Prod1").declare_master("Rout5")
+FM("Prod2").declare_master("Rout8")
+
+update_UI()
+###### Task Scheduler ######
+def Run_task_scheduler():
+    global clock_tick
+    if(clock_tick % 6 == 0):
+        FM("Prod1").produce_message(['f','i','z','z'],"Con2")
+    elif(clock_tick % 5 == 0):
+        FM("Prod0").produce_message(['b','u','z','z'],"Con0")
+
+
+
+################ THE CLOCK ################
+def on_spacebar(event):
+    global clock_tick
+    if event.name == 'space':
+        Run_task_scheduler()
+        clock_tick += 1
+        for x in Routers:
+            x.hold_on()
+        for x in Producers:
+            x.hold_on()
+        for x in Routers:
+            x.send()
+        for x in Producers:
+            x.send()
+        update_UI()
+keyboard.on_press_key('space', on_spacebar)
+keyboard.wait('esc')
