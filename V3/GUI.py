0a6780770caa81f2ebb62dec3db6575e7a6e8f2d
Tom <sc21tpc@leeds.ac.uk>
2024-03-07 13:46:39 +0000

    GUI Added

---
+++ V3/GUI.py
@@ -0,0 +1,17 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QHBoxLayout
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0,0,640,480)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
+
+main()
