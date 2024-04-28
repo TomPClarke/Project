b95b4b4b07444a14b55942ac36178bc9469b124b
ejpogp <drg@fsef.com>
2024-04-28 20:39:12 +0100

    6x6 mesh added

--- V3/NODES.py
+++ V3/NODES.py
@@ -14,10 +14,18 @@ Producers can send data, they can also path-find.
 A router has a name, a routing table, a direction and a slave.
 The direction is the entry in the routing to which the router will send all flits
 
+
+
+
 """
-
-
+# 3x3 Mesh
 Consumers = [Consumer("Con0"),Consumer("Con1"),Consumer("Con2"),Consumer("Con3")]
 Producers = [Producer("Prod0"),Producer("Prod1"),Producer("Prod2"),Producer("Prod3"),Producer("Prod4")]
 Routers = [Router("Rout0"),Router("Rout1"),Router("Rout2"),Router("Rout3"),
            Router("Rout4"),Router("Rout5"),Router("Rout6"),Router("Rout7"), Router("Rout8")]
+# 6x6 Mesh
+Consumers = [Consumer(f"Con{i}") for i in range(16)]
+Producers = [Producer(f"Prod{i}") for i in range(20)]
+Routers = [Router(f"Rout{i}") for i in range(36)]
+for i in Producers:
+    print(i.name)
